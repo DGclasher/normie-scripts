c2fb317c8aab9b66142722ce73d6e34d21987be6
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-09-01 16:58:39 +0530

    gdb-installer

--- gdb-installer/gdb-installer.sh
+++ gdb-installer/gdb-installer.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-echo "Downloading GDB source"
 gdb_version=$(curl -s https://ftp.gnu.org/gnu/gdb/ | grep -o  'gdb.*\.tar\.gz"' | sed 's/"//g' | tail -n 3 | head -n 1)
-wget "https://ftp.gnu.org/gnu/gdb/$gdb_version" -O "/tmp/$gdb_version"
-
-echo "Extracting GDB"
-cd /tmp
-tar xf "$gdb_version"
 gdb_folder=$(echo "$gdb_version" | sed 's/\.tar\.gz//g')
-cd "$gdb_folder"
 
+echo "Checking if gdb version already exists"
+if [ ! -d "/opt/$gdb_folder" ]
+then
+    echo "Downloading GDB source"
+    sudo wget "https://ftp.gnu.org/gnu/gdb/$gdb_version" -O "/opt/$gdb_version"
+
+    echo "Extracting GDB"
+    cd /opt
+    sudo tar xf "$gdb_version"
+    sudo rm "$gdb_version"
+fi
+
+cd "/opt/$gdb_folder"
 echo "Making and Installing GDB"
-sh configure --with-python
-make
+sh configure --with-python=/usr/bin/python3
+sudo make
 sudo make install
 
 echo "Installation Finished"
